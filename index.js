9f761bea070a7409ba30dde80beaff1cde0801c5
piyushMobulous <piyush.verma@mobulous.com>
2025-03-13 15:49:48 +0530

    chnge

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const winston = require("winston");
 
-const connectDB = require("./src/config/db");
+// const connectDB = require("./src/config/db");
 const authRoutes = require("./src/routes/authRoutes");
 const eventRoutes = require("./src/routes/eventRoutes");
 const tradeRoutes = require("./src/routes/tradeRoutes");
@@ -18,9 +18,10 @@ const { fetchLiveData } = require("./src/services/fetchDataService");
 const { setupSockets } = require("./src/services/socketService");
 // const authenticateUser = require("./src/middleware/authMiddleware");
 // const { authenticateUser } = require("./src/middleware/authMiddleware");
-const { authenticateUser, adminOnly } = require("./src/middleware/authMiddleware");
-
-
+const {
+  authenticateUser,
+  adminOnly,
+} = require("./src/middleware/authMiddleware");
 
 dotenv.config();
 
@@ -28,7 +29,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: "*" } });
 
-connectDB();
+// connectDB();
 setupLogging();
 
 app.use(express.json());
@@ -46,28 +47,31 @@ setupSockets(io);
 setInterval(fetchLiveData, 30000);
 
 app.use((req, res) => {
-    res.json({
-      error: "404",
-      message: "Route you were looking for was not found",
-    });
+  res.json({
+    error: "404",
+    message: "Route you were looking for was not found",
   });
+});
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 
 function setupLogging() {
-    const logger = winston.createLogger({
-        level: "info",
-        format: winston.format.json(),
-        transports: [
-            new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-            new winston.transports.File({ filename: "logs/combined.log" })
-        ]
-    });
-    if (process.env.NODE_ENV !== "production") {
-        logger.add(new winston.transports.Console({ format: winston.format.simple() }));
-    }
+  const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({
+        filename: "logs/error.log",
+        level: "error",
+      }),
+      new winston.transports.File({ filename: "logs/combined.log" }),
+    ],
+  });
+  if (process.env.NODE_ENV !== "production") {
+    logger.add(
+      new winston.transports.Console({ format: winston.format.simple() })
+    );
+  }
 }
-
-
